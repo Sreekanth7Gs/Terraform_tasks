2abd3c3638c0eed308b74c3ecb3c2dbd2ad355e1
sree <sree@gmail.com>
2023-12-05 13:05:59 +0530

    modified

--- vpc.tf
+++ vpc.tf
@@ -10,6 +10,7 @@ resource "aws_subnet" "sn-1" {
     vpc_id = aws_vpc.terraform_vpc.id
     cidr_block = "10.0.0.0/24"
     availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sn-2" {
