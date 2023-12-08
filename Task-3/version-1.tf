234066a73349f6cdb8c581a9f709ae3208113961
sree <sree@gmail.com>
2023-12-08 13:31:55 +0530

    code is modified

--- Task-3/version-1.tf
+++ Task-3/version-1.tf
@@ -6,14 +6,3 @@ provider "aws" {
 resource "aws_s3_bucket" "tf-bucket-4" {
   bucket = "tf-bucket-4"
 }
-
-
-resource "aws_s3_bucket_public_access_block" "tf-bucket-4" {
-  bucket = aws_s3_bucket.tf-bucket-4.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
